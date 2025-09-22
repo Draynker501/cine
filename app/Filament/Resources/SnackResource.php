05758c33108af87615027a5abc5fc5fd7a45fe54
Diego Castaneda <cardsm713@gmail.com>
2025-09-21 23:35:41 -0600

    Modulo de Snacks semicompleto

--- app/Filament/Resources/SnackResource.php
+++ app/Filament/Resources/SnackResource.php
@@ -17,7 +17,7 @@ class SnackResource extends Resource
 {
     protected static ?string $model = Snack::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'phosphor-popcorn';
 
     public static function form(Form $form): Form
     {
