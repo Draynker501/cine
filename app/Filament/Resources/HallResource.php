8fce1a47ed193e80c3343b33b626b3fd65682de1
Diego Castaneda <cardsm713@gmail.com>
2025-10-08 18:21:34 -0600

    correccion en icono de hall

--- app/Filament/Resources/HallResource.php
+++ app/Filament/Resources/HallResource.php
@@ -17,7 +17,7 @@ class HallResource extends Resource
 {
     protected static ?string $model = Hall::class;
 
-    protected static ?string $navigationIcon = 'phosor-hall';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
